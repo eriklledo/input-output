e80b16db5b6ec0fadbf4242edf7efa3b04419af0
Èrik Lledó <lledoerik@gmail.com>
2025-04-25 10:57:50 +0200

    ex1

--- src/ex1.java
+++ src/ex1.java
@@ -1,15 +1,35 @@
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class ex1 {
     public static void main(String[] args) throws IOException {
-        Path directory = Paths.get("/home/sonwerik/file_manager/directory");
-        Path file = Paths.get("/home/sonwerik/file_manager/directory/file.txt");
+        Path directory = Paths.get("directory");
+        Path file = directory.resolve("file.txt");
+        Path file2 = directory.resolve("file2.txt");
 
-        Files.createDirectory(directory);
-        Files.createFile(file);
-        Files.writeString(file, "Miau!");
+        if (!Files.exists(directory)) {Files.createDirectory(directory);}
+        if (!Files.exists(file)) {Files.createFile(file);}
+        if (!Files.exists(file2)) {Files.createFile(file2);}
+
+        Files.writeString(file, "Aquest és el fitxer gran!");
+        Files.writeString(file2, "Aquest n'és el petit");
+
+        Path fitxerMesGran = null;
+        long midaMesGran = 0;
+
+        DirectoryStream<Path> stream = Files.newDirectoryStream(directory);
+        for (Path fitxer : stream) {
+            if (Files.isRegularFile(fitxer)) {
+                long size = Files.size(fitxer);
+                if (size > midaMesGran) {
+                    midaMesGran = size;
+                    fitxerMesGran = fitxer;
+                }
+            }
+        }
+        System.out.println(fitxerMesGran);
     }
 }
